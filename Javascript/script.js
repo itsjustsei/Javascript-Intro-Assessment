ac7be359cdc44cb9f20ba25b3e42cc3238b6edba
Ajarion Morales <moralesajarion@gmail.com>
2022-10-18 01:07:35 -0400

    first commit

---
+++ Javascript/script.js
@@ -0,0 +1,8 @@
+var firstName;
+firstName = "Ajarion";
+var lastName;
+lastName = "Morales";
+var age = 19;
+alert("My name is " + firstName);
+alert("I am " + age + " years old");
+console.log("I am having fun figuring things out")
